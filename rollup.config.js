28625ffc847d15fb876c151ad3985da76950c40a
Manassarn Manoonchai <manassarn@gmail.com>
2020-08-15 21:49:02 +0700

    :heavy_plus_sign: Setup Tailwind

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,13 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import sveltePreprocess from 'svelte-preprocess'
 
 const production = !process.env.ROLLUP_WATCH;
 
 function serve() {
 	let server;
-	
+
 	function toExit() {
 		if (server) server.kill(0);
 	}
@@ -43,7 +44,9 @@ export default {
 			// a separate file - better for performance
 			css: css => {
 				css.write('public/build/bundle.css');
-			}
+      },
+
+      preprocess: sveltePreprocess({ postcss: true }),
 		}),
 
 		// If you have external dependencies installed from
--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,14 @@
 <script>
 	export let name;
+  import Tailwind from './Tailwind.svelte'
 </script>
+
+<Tailwind />
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <p class="tailwind">Now with Tailwind!</p>
 </main>
 
 <style>
@@ -21,6 +25,10 @@
 		font-size: 4em;
 		font-weight: 100;
 	}
+
+  .tailwind {
+    @apply text-3xl bg-red-500;
+  }
 
 	@media (min-width: 640px) {
 		main {
